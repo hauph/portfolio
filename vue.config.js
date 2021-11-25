3ecbff2c9b1f15f5d6605aea334c7860e3dbf640
Hau Pham <hauph@hotmail.com>
2021-11-25 09:38:24 +0700

    update publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'
   pwa: {
     // configure the workbox plugin
     workboxPluginMode: "InjectManifest",
